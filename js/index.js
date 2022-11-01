21405784be8c9fce007f62a9b478fadf2f276ca0
Baek MinKyung <en0008@naver.com>
2022-11-01 17:37:37 +0900

    0.1.2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(window).scroll(function(){
 
-        gsap.registerPlugin(ScrollTrigger);
+        /* gsap.registerPlugin(ScrollTrigger);
         gsap.to(".h1_img", {
         scrollTrigger: {
             trigger: ".trigger",
@@ -9,8 +9,50 @@ $(document).ready(function(){
             scrub: true,
         },
         scale: 0.05, duration: 5,
-        });
+        }); */
         
+        console.log($(window).scrollTop())
+
+        if($(window).scrollTop() > 200)
+        {
+            $('.copy_text1').css('color', '#fff');
+        } else
+        {
+            $('.copy_text1').css('color', 'rgba(255, 255, 255, 0.4)');
+        }
+        if($(window).scrollTop() > 400)
+        {
+            $('.copy_text2').css('color', '#fff');
+            $('.copy_text1').css('color', 'rgba(255, 255, 255, 0.4)');
+        } else
+        {
+            $('.copy_text2').css('color', 'rgba(255, 255, 255, 0.4)');
+        }
+        if($(window).scrollTop() > 600)
+        {
+            $('.copy_text3').css('color', '#fff');
+            $('.copy_text2').css('color', 'rgba(255, 255, 255, 0.4)');
+        } else
+        {
+            $('.copy_text3').css('color', 'rgba(255, 255, 255, 0.4)');
+        }
+        if($(window).scrollTop() > 800)
+        {
+            $('.copy_text4>img').css('opacity', '1');
+            $('.copy_text3').css('color', 'rgba(255, 255, 255, 0.4)');
+        } else
+        {
+            $('.copy_text4>img').css('opacity', '0.4');
+        }
+        if($(window).scrollTop() > 1200)
+        {
+            $('.title_text').css('margin-top', 'calc(100vw * -800 / 1920)');
+        } else
+        {
+            $('.title_text').css('margin-top', 'calc(100vw * 144 / 1920)');
+        }
+        
+
         if($(window).scrollTop() > 1800)
         {
             $(".overview").children(".subject").css('position', 'sticky');
@@ -52,8 +94,28 @@ $(document).ready(function(){
             $(".first_span").css('opacity', '1');
         }
 
-        $('.color_slide').slick({
-            dots: true
-        });
     });
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    // console.log("ok");
+    window.onscroll = function(){
+        // console.log(document.querySelector("html").scrollTop);
+        if(document.querySelector("html").scrollTop > 700){
+            document.querySelector("header").style.top = "-80px";
+        } else {
+            document.querySelector("header").style.top = "0";
+        }
+    }
+    window.onmousemove = function(e){
+        //console.log(e.clientY);
+        if(e.clientY < 100){
+            document.querySelector("header").style.top = "0";
+        } else if(document.querySelector("html").scrollTop > 700) {
+            document.querySelector("header").style.top = "-80px";
+        }
+         else {
+            document.querySelector("header").style.top = "-80px";
+        }
+    }
 });
